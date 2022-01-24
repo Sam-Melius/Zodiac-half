3353d94a69476db1be810982c5c4ce5f8d027cf3
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-24 16:10:11 -0700

    not working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css'
 {/* import the zodiac array */}
+import Header from './Header/Header.js'
+import Main from './Main/Main.js'
+import Footer from './Footer/Footer.js'
 
-function App() {
+
+function App({ zodiac }) {
   return (
-    <div className="App">
+    <div className="App"> 
       {/* import and use the Header. Main, and Footer components here */}
+
       {/* the Header component needs a string passed in as the `name` prop */}
+      <Header name='Sam Melius' />
       {/* the Footer component needs a year passed in as the `year` prop */}
+      <Footer year='2022' />
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
+      <Main zodiac={zodiac} />
     </div>
   )
 }
